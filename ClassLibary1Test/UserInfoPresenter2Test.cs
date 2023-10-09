df8d3225c3a56d32b6c007a8b8c0db7beaa969bd
KotovIN <KotovIN@dom201.local>
2023-10-09 14:28:29 +0700

    testAdding

---
+++ ClassLibary1Test/UserInfoPresenter2Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibary1Test
+{
+    [TestClass]
+    public class UserInfoPresenter2Test
+    {
+        [TestMethod]
+        public void GenderTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ResidenceTest()
+        {
+
+        }
+    }
+}
